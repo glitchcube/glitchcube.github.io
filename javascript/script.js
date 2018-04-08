360543a8efa9956da4c34a7be76ee8f50c083ad2
Frank Navarsete <frank.navarsete@gmail.com>
2018-04-08 15:57:35 +0200

    Section 10 lecture 83: Function + exercise

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,46 @@
-4 + 3;
-
-if(4 + 3 === 7) {
-	alert("You're smart!");
+function sayHello() {
+	console.log("Hello");
 }
+
+sayHello();
+
+var sayBye = function() {
+	console.log("Bye");
+}
+
+sayBye();
+
+function sing(song) {
+	console.log(song);
+}
+
+sing("Laaa deee daaa");
+sing("hellllloooooo");
+sing("backstreet back alright");
+
+
+// Exercise
+var age = prompt("What is your age?");
+function checkDriverAge(age) {
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+
+checkDriverAge(age);
+
+/*var checkDriverAge2 = function() {
+	var age = prompt("What is your age?");
+
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}*/

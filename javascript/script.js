89d5ef4ae182f0ea8f226267997498414e44c31d
Frank Navarsete <frank.navarsete@gmail.com>
2018-04-14 11:21:49 +0200

    Section 10 lecture 88 objects and arrays for facebook app

--- javascript/script.js
+++ javascript/script.js
@@ -95,3 +95,33 @@ you should have at the end:
  var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 // access "Oranges".
 array2[1][1][0];
+
+
+//// ------------- Exercise 7 "facebook" -----------/////
+
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+var user = {
+	username: "frank",
+	password: "myPass"
+};
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+var database = [user];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+	{
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeetyy cool!"
+	}
+];

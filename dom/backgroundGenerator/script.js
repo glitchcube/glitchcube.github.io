6d41c03a058168f0a3024e9119557ee31819d0fb
Frank Navarsete <frank.navarsete@gmail.com>
2018-04-23 19:42:16 +0200

    Section 10 lecture 101 Background generator files

--- dom/backgroundGenerator/script.js
+++ dom/backgroundGenerator/script.js
@@ -2,9 +2,26 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomBtn = document.querySelector("button");
 
+randomBtn.addEventListener("click", function() {
+	body.style.background = "linear-gradient(to right, " 
+	+ generateRandomRGB() 
+	+ ", " 
+	+ generateRandomRGB() 
+	+ ")";
+})
+
+function generateRandomRGB() {
+	var rgbColor =  "rgb(" + Math.round(Math.random()*255) + "," + Math.round(Math.random()*255) + "," + Math.round(Math.random()*255) + ")"
+	console.log("RGB color: " + rgbColor);
+	return rgbColor;
+};
+
+var color = random_rgba();
 
 function setGradient() {
+	console.log(color2.value)
 	body.style.background ="linear-gradient(to right, " 
 	+ color1.value 
 	+ ", " 
@@ -13,6 +30,8 @@ function setGradient() {
 
 	css.textContent = body.style.background + ";";
 }
+
+setGradient();
 
 color1.addEventListener("input", setGradient);
 
